32301b693a7da492be2a68cfa53394bd6f9e87c9
tonnhgcs18328 <58252442+tonnhgcs18328@users.noreply.github.com>
2020-06-29 01:32:01 +0700

    Update product.php

--- product.php
+++ product.php
@@ -1,10 +1,6 @@
 <?php
 	$link = pg_connect("
-	host=ec2-34-232-147-86.compute-1.amazonaws.com
-  dbname=d3c1oim74q14mm
-  user=lzggidtdhppjik
-  port=5432
-  password=1dc2f80f177ddb0989962d06593d755e5b7d610dc2f71731b3910e002e326d57
+	host=localhost
 	");
 
 	if($link === false){
